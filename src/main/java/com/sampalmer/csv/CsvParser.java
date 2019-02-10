8d8cf2ea673ced317bb50f9688525e7975009035
MancunianSam <sam.palmer2@protonmail.com>
2019-02-10 10:10:05 +0000

    Implementation of getHeaderFromRows

--- src/main/java/com/sampalmer/csv/CsvParser.java
+++ src/main/java/com/sampalmer/csv/CsvParser.java
@@ -1,10 +1,16 @@
 package com.sampalmer.csv;
 
+import com.sampalmer.exceptions.EmptyCsvFileException;
+
 import java.util.stream.Stream;
 
 public class CsvParser {
 
 	public Stream<String> getHeaderFromRows(Stream<String> rows) {
-		return Stream.of();
+		String[] columnHeaders = rows.findFirst()
+				.map(r -> r.split(","))
+				.orElseThrow(EmptyCsvFileException::new);
+
+		return Stream.of(columnHeaders).map(String::trim);
 	}
 }
